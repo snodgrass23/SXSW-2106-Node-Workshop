e7e8ccdc6a95994ab2790b40068dcc545fa2bfdc
Jim Snodgrass <snodgrass23@gmail.com>
2016-02-21 10:57:38 -0500

    add basicWeb service using express.js

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
 "use strict";
 
 // run all services
-const SERVICES = new Set(['helloWorld']);
+const SERVICES = new Set([
+  'helloWorld',
+  'basicWeb'
+]);
 
 for (let service of SERVICES) {
   require(`./services/${service}`);
---
+++ services/basicWeb/app.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+
+app.listen(3000, function () {
+  console.log('Basic Web Service listening on port 3000!');
+});
